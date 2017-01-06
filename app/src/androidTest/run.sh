4643c7f0facbb583152437353416bcfad5f3c458
xxping90 <xxping90@126.com>
2017-01-06 16:34:57 +0800

    run.sh

---
+++ app/src/androidTest/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+testautoRootPath=/sdcard/Robotium
+junitReportPath=$testautoRootPath/report
+
+cd   todolist
+echo “uninstall APK and TestAPK”
+adb uninstall com.example.todolist
+adb uninstall com.example.todolist.tests
+
+echo “install APK and TestAPK”
+adb install app/build/outputs/apk/app-debug.apk
+adb install app/build/outputs/apk/app-debug-androidTest.apk
+
+echo “start to run test”
+
+adb shell am instrument -w -e reportDir $junitReportPath  -e reportFile test-report.xml com.example.todolist.tests/com.example.todolist.runners.Runner
+
+echo  “pull junit report”
+
+adb pull junitReportPath/test-report.xml  $WORKSPACE
