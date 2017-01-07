89f34193ad2e24d35a5da116078e86d6218e2fdc
xxping90 <xxping90@126.com>
2017-01-07 13:47:56 +0800

    adb -s

--- app/src/androidTest/run.sh
+++ app/src/androidTest/run.sh
@@ -3,22 +3,22 @@ testautoRootPath=/sdcard/Robotium
 junitReportPath=$testautoRootPath/report
 
 echo "delete all screenshots in sdcard"
-adb shell rm -rf /sdcard/Robotium-Screenshots/*
+adb -s $1 shell rm -rf /sdcard/Robotium-Screenshots/*
 
 echo “uninstall APK and TestAPK”
-adb uninstall com.example.todolist
-adb uninstall com.example.todolist.tests
+adb -s $1 uninstall com.example.todolist
+adb -s $1 uninstall com.example.todolist.tests
 
 echo “install APK and TestAPK”
-adb install app/build/outputs/apk/app-debug.apk
-adb install app/build/outputs/apk/app-debug-androidTest.apk
+adb -s $1 install app/build/outputs/apk/app-debug.apk
+adb -s $1 install app/build/outputs/apk/app-debug-androidTest.apk
 
 echo “start to run test”
 
-adb shell am instrument -w -e reportDir $junitReportPath  -e reportFile test-report.xml com.example.todolist.tests/com.example.todolist.test.runners.Runner
+adb -s $1 shell am instrument -w -e reportDir $junitReportPath  -e reportFile test-report.xml com.example.todolist.tests/com.example.todolist.test.runners.Runner
 
 echo  “pull junit report”
 
-adb pull $junitReportPath/test-report.xml  $WORKSPACE
+adb -s $1 pull $junitReportPath/test-report.xml  $WORKSPACE
 
-adb pull /sdcard/Robotium-Screenshots/  $WORKSPACE/
+adb -s $1 pull /sdcard/Robotium-Screenshots/  $WORKSPACE/
