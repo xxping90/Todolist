26301c4fa6bc908d040c07e4c19d82bacf4da997
xxping90 <xxping90@126.com>
2017-01-15 23:56:46 +0800

    crash11

--- app/src/androidTest/run.sh
+++ app/src/androidTest/run.sh
@@ -29,16 +29,16 @@ echo "start to run test"
 loop=true
 #设置一个变量用例表明是否需要重新组织测试用例集
 #如果发生crash，就把这个值设置为true，重新组织测试用例集，去掉已经运行的case列表
-regeneratTestsuite=false
+regenerateTestsuite=false
 #下面的变量用来统计crash发生的次数，并且用于命名crash报告，确保报告名唯一
 crashCount=0
 #开始循环体内容，因为loop的初始值为true，所以第一次肯定进入循环体
 while [ $loop == "true" ]
 do
 #指定junit产生的路径为$junitReportPath ,并指定报告的名称为"junit-report_"+crashCount+".xml"
-#通过-e 添加一个键为regenerateTestsuite,值为$regeneratTestsuite  ,标识是否重新组织测试用例集合
-#第一次regeneratTestsuite的值为false，如果crash发生了，就需要把这个值赋值为true，下次循环时这个值就是true了。需要组织测试用例
-    adb -s $1 shell am instrument -w -e reportDir $junitReportPath  -e reportFile junit-report_${crashCount}.xml  -e regeneratTestsuite $regeneratTestsuite com.example.todolist.tests/com.example.todolist.test.runners.Runner
+#通过-e 添加一个键为regenerateTestsuite,值为$regenerateTestsuite  ,标识是否重新组织测试用例集合
+#第一次regenerateTestsuite的值为false，如果crash发生了，就需要把这个值赋值为true，下次循环时这个值就是true了。需要组织测试用例
+    adb -s $1 shell am instrument -w -e reportDir $junitReportPath  -e reportFile junit-report_${crashCount}.xml  -e regenerateTestsuite $regenerateTestsuite com.example.todolist.tests/com.example.todolist.test.runners.Runner
 #执行完一次之后。先把junit包报告pull到jenkins workspace之下
 #同时把crash.txt文件pull到workplace之下
     echo  "pull junit report"
@@ -46,7 +46,7 @@ do
     adb -s $1 pull $junitReportPath/junit-report_${crashCount}.xml  $WORKSPACE/junit-report_${crashCount}.xml
     adb -s $1 pill $testautoRootPath/crash.txt    $WORKSPACE
 #现在判断workspace地下是否有没有crash.txt文件
-#有的话，说明发生了crash，需要把regeneratTestsuite的值赋值为true，重新组织用例，排除已经运行的用例
+#有的话，说明发生了crash，需要把regenerateTestsuite的值赋值为true，重新组织用例，排除已经运行的用例
 #并把crashCount的值加1，代表发生的crash数
 #把crash.txt的内容追加到totalCrash.txt,便于统计所有的crash用例
 #并删除workspace 下的crash.txt，避免干扰后面产生的文件
@@ -55,7 +55,7 @@ do
         echo "crash happen"
         /usr/bin/cat $WORKSAPCE\\crash.txt >> totalCrash.txt
         /usr/bin/rm -f $WORKSPCACE/crash.txt
-        regeneratTestsuite=true
+        regenerateTestsuite=true
         ((crashCount=$crashCount+1))
     else
         loop=false
